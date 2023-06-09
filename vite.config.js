4306203b11ba3766c9ba8389ed372d14063c6c60
rakesh murali <rakesh.m2@samsung.com>
2023-06-09 18:28:42 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 9000,
+    open: true,
+    proxy: {
+      '/dev': {
+        target: 'http://localhost:9001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/dev/, ''),
+      },
+    },
+  },
 })
